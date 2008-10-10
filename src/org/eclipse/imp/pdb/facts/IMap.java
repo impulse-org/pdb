0ab84f448bffb0115c30575dfaad704bae40af4c
Jurgen J. Vinju <jurgen.vinju@cwi.nl>
2008-10-10 12:42:42 +0000

    Added IMap interfaces

---
+++ src/org/eclipse/imp/pdb/facts/IMap.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+* Copyright (c) 2007 IBM Corporation.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*    jurgen@vinju.org
+
+*******************************************************************************/
+
+package org.eclipse.imp.pdb.facts;
+
+import org.eclipse.imp.pdb.facts.type.FactTypeError;
+
+public interface IMap extends Iterable<IValue>, IValue {
+    public boolean isEmpty();
+
+    public int size();
+
+    public IMapWriter getWriter();
+    
+    public int arity();
+
+    public IMap put(IValue key, IValue value) throws FactTypeError ;
+    
+    public IValue get(IValue key);
+
+    public boolean containsKey(IValue key) throws FactTypeError ;
+    
+    public boolean containsValue(IValue value) throws FactTypeError ;
+}
