8be6c4ec5b59915fb25137d3f232b9ed60cc9c77
Robert M. Fuhrer <rmfuhrer@optonline.net>
2008-10-31 22:17:41 +0000

    Removed unnecessary import.

--- src/org/eclipse/imp/pdb/analysis/IFactGenerator.java
+++ src/org/eclipse/imp/pdb/analysis/IFactGenerator.java
@@ -18,7 +18,6 @@ package org.eclipse.imp.pdb.analysis;
 import org.eclipse.imp.pdb.facts.db.FactBase;
 import org.eclipse.imp.pdb.facts.db.IFactContext;
 import org.eclipse.imp.pdb.facts.type.Type;
-import org.eclipse.imp.pdb.facts.type.TypeFactory;
 
 public interface IFactGenerator {
     void generate(FactBase factBase, Type type, IFactContext context) throws AnalysisException;
