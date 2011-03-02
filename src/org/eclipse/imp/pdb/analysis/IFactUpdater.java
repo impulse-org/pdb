ae2231bc8e4b65f521743b51a74ebfd83e65dd7d
Robert M. Fuhrer <rmfuhrer@optonline.net>
2011-03-02 23:48:08 +0000

    Added new flavor of update() method to handle changes to open documents. Added a new arg to the existing flavor of update() to take a set of document working copies.

--- src/org/eclipse/imp/pdb/analysis/IFactUpdater.java
+++ src/org/eclipse/imp/pdb/analysis/IFactUpdater.java
@@ -1,13 +1,18 @@
 package org.eclipse.imp.pdb.analysis;
 
+import java.util.Map;
+
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.imp.pdb.facts.db.FactBase;
 import org.eclipse.imp.pdb.facts.db.IFactContext;
 import org.eclipse.imp.pdb.facts.type.Type;
+import org.eclipse.imp.pdb.indexing.IndexedDocumentDescriptor;
+import org.eclipse.jface.text.IDocument;
 
 /**
  * This interface should be implemented by clients that wish to keep facts
- * in the PDB up to date as workspace resources change.
+ * in the PDB up to date as workspace resources or open {@link IDocument}s change.
  * @author rfuhrer@watson.ibm.com
  */
 public interface IFactUpdater {
@@ -15,6 +20,16 @@ public interface IFactUpdater {
      * Update the given FactBase with a fact of the given type and fact context,
      * given a change to the given resource.
      * @throws AnalysisException
+     * @param changeKind a valid value as returned by {@link IResourceDelta#getKind()}
      */
-    void update(FactBase factBase, Type type, IFactContext context, IResource res) throws AnalysisException;
+    void update(FactBase factBase, Type type, IFactContext context, IResource res, int changeKind,
+                Map<IResource, IndexedDocumentDescriptor> workingCopies) throws AnalysisException;
+
+    /**
+     * Update the given FactBase with a fact of the given type and fact context,
+     * given a change to the document corresponding to the given resource.
+     * @throws AnalysisException
+     */
+    void update(FactBase factBase, Type type, IFactContext context, IResource res,
+                Map<IResource, IndexedDocumentDescriptor> workingCopies) throws AnalysisException;
 }
