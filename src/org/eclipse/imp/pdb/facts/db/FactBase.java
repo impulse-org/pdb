5d9c98cf3aadaa660591f868e357b4c0c7c17781
Robert M. Fuhrer <rmfuhrer@optonline.net>
2011-03-02 23:46:54 +0000

    Made FactBase no longer a singleton, so that clients can create them to suit their own contexts and manage their lifetimes as they see fit.

--- src/org/eclipse/imp/pdb/facts/db/FactBase.java
+++ src/org/eclipse/imp/pdb/facts/db/FactBase.java
@@ -1,14 +1,13 @@
 /*******************************************************************************
-* Copyright (c) 2007 IBM Corporation.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
-
-*******************************************************************************/
+ * Copyright (c) 2007 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
+ *******************************************************************************/
 
 package org.eclipse.imp.pdb.facts.db;
 
@@ -35,7 +34,7 @@ public class FactBase {
 	
 	private Map<IFactKey, IValue> fFactDatabase= new HashMap<IFactKey, IValue>();
 
-        private IFactKey masterKey = new IFactKey() {
+	private IFactKey masterKey = new IFactKey() {
 		public IFactContext getContext() {
 			throw new UnsupportedOperationException("masterKey");
 		}
@@ -45,7 +44,7 @@ public class FactBase {
 		}
 	};
 	
-	private FactBase() {
+	public FactBase() {
 	}
 	
 	private HashMap<IFactKey, Set<IFactBaseListener>> fListenerMap = new HashMap<IFactKey, Set<IFactBaseListener>>();
